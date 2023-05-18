91aa1c962e97cc7a65cb30b277ea7c2163e1598e
Srihari <sshsrn@gmail.com>
2023-05-18 21:57:08 +0530

    feat: integrated AI
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const axios = require('axios');
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
-var cron = require('node-cron');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
+
+const prefix = process.env.REQ_BODY_CONTENT_PREFIX;
+const suffix = process.env.REQ_BODY_CONTENT_SUFFIX;
 
 bot.start((ctx) => ctx.reply(`Welcome ${ctx.chat.first_name}\n\nI am a bot designed by Srihari S with the functionalities of Bard AI by Google.`));
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.on('text', async (ctx) => {
+    console.log(encodeURI(ctx.message.text));
+    const payload = prefix + encodeURI(ctx.message.text) + suffix;
+    console.log("payload: ", payload);
+    const response = await axios.post(process.env.BARD_API_ENDPOINT, payload, {
+        headers: {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-length': payload.length,
+            'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'cookie': process.env.BARD_COOKIE,
+            'origin': 'https://bard.google.com',
+            'referer': 'https://bard.google.com/'
+        }
+    })
+    .then((response) => {
+        console.log("response is: ",response.data);
+        const maxMsgLen = 4000;
+        const messages = [];
+        for (let i = 0; i < response.data.length; i += maxMsgLen) {
+            messages.push(response.data.substring(i, i + maxMsgLen));
+        }
+        messages.forEach((message) => {
+            ctx.reply(message);
+        });
+    })
+    .catch((error) => {
+        console.log("error: ", error);
+    });
+});
 
 // Launch the bot
 bot.launch();
