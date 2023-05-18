8fafd6c2f5980a52fd3ef6d60c79b0ee1332141c
Srihari <sshsrn@gmail.com>
2023-05-18 21:05:31 +0530

    feat: added boilerplate code
    Signed-off-by: Srihari <sshsrn@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const axios = require('axios');
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+var cron = require('node-cron');
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
+
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.chat.first_name}\n\nI am a bot designed by Srihari S with the functionalities of Bard AI by Google.`));
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+// Launch the bot
+bot.launch();
